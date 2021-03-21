ff0f5a4f3f5a2872f7541eb190181d92c157f090
unknown <singhsharandeep676@gmail.com>
2021-03-20 18:31:34 -0700

    added redux dev tools

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const composeEnchancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnchancers(applyMiddleware()));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
